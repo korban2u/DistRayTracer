b243804897225cf2736f03936629c451e887495b
Yeahboys04 <ryankorban2004@gmail.com>
2025-05-30 13:24:56 +0200

    Replacement des dossier + ajout Raytracing

--- src/shared/ServiceNoeud.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-import raytracer.Image;
-import raytracer.Scene;
-
-interface ServiceNoeud extends Remote{
-  public Image calcul(Scene scene, int x0,int y0,int l,int h) throws RemoteException;
-}
